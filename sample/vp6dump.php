a5816ff162ac9ed228e6d2db102f678810265ab1
Yoshihiro Yamazaki <yoya@awm.jp>
2020-10-05 15:12:53 +0900

    use IP_VP6_EA. for VP6 in EA container.

--- sample/vp6dump.php
+++ sample/vp6dump.php
@@ -4,6 +4,7 @@ if (is_readable('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 } else {
     require_once 'IO/VP6.php';
+    require_once 'IO/VP6/EA.php';
 }
 
 $options = getopt("f:");
@@ -22,14 +23,30 @@ $vp6data = file_get_contents($filename);
 
 $opts = [];
 
-$vp6 = new IO_VP6();
-try {
-    $vp6->parse($vp6data, $opts);
-} catch (Exception $e) {
-    echo "ERROR: vp6dump: $filename:".PHP_EOL;
-    echo $e->getMessage()." file:".$e->getFile()." line:".$e->getLine().PHP_EOL;
-    echo $e->getTraceAsString().PHP_EOL;
-    exit (1);
+$vp6dataArr = [];
+
+if (IO_VP6_EA::is_ea($vp6data)) {
+    $ea = new IO_VP6_EA();
+    $ea->parse($vp6data);
+    $ea->dump($vp6data);
+    foreach ($ea->getFrames() as $frame) {
+        $vp6dataArr []= $frame["Data"];
+    }
+} else {
+    $vp6dataArr []= $vp6data;
 }
 
-$vp6->dump($opts);
+foreach ($vp6dataArr as $idx => $vp6data) {
+    echo "[$idx]\n";
+    $vp6 = new IO_VP6();
+    try {
+        $vp6->parse($vp6data, $opts);
+    } catch (Exception $e) {
+        echo "ERROR: vp6dump: $filename:".PHP_EOL;
+        echo $e->getMessage()." file:".$e->getFile()." line:".$e->getLine().PHP_EOL;
+        echo $e->getTraceAsString().PHP_EOL;
+        exit (1);
+    }
+    $vp6->dump($opts);
+}
+
